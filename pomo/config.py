e36d46205da7a86d2d7206b60567d48ea23510dd
Bogdan Kirillov <bakirillov1488@gmail.com>
2020-04-22 14:21:13 +0300

    defaults for getenv

--- pomo/config.py
+++ pomo/config.py
@@ -18,10 +18,10 @@ class Config:
 
 
 config = Config(
-    int(os.getenv("POMO_WORK_TIME")),
-    int(os.getenv("POMO_SHORT_REST")),
-    int(os.getenv("POMO_LONG_REST")),
-    int(os.getenv("POMO_SESSIONS")),
-    Path(os.getenv("POMO_WORK_SOUND")),
-    Path(os.getenv("POMO_REST_SOUND")),
+    int(os.getenv("POMO_WORK_TIME", default="25")),
+    int(os.getenv("POMO_SHORT_REST", default="5")),
+    int(os.getenv("POMO_LONG_REST", default="15")),
+    int(os.getenv("POMO_SESSIONS", default="2")),
+    Path(os.getenv("POMO_WORK_SOUND", "../work.mp3")),
+    Path(os.getenv("POMO_REST_SOUND", "../work.mp3")),
 )
